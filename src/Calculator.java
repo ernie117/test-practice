c91e308bdc9e9fedd25121f0a912404569121a03
Robert Matthew Stephens <matt.stephens@dvla.gov.uk>
2019-08-18 13:21:44 +0100

    initial commit

---
+++ src/Calculator.java
@@ -0,0 +1,30 @@
+class Calculator {
+
+    static int sum(int operandOne, int operandTwo) {
+        return operandOne + operandTwo;
+    }
+
+    static int subtract(int operandOne, int operandTwo) {
+        return operandOne - operandTwo;
+    }
+
+    static int division(int operandOne, int operandTwo) {
+        return operandOne / operandTwo;
+    }
+
+    static int multiply(int operandOne, int operandTwo) {
+        return operandOne * operandTwo;
+    }
+
+    static int complicatedMethod(int operandOne, int operandTwo) {
+        int firstResult = Calculator.sum(operandOne, operandTwo);
+
+        int secondResult = Calculator.subtract(firstResult, operandOne);
+
+        int thirdResult = Calculator.division(secondResult, operandTwo);
+
+        int finalResult = Calculator.multiply(secondResult, thirdResult);
+
+        return finalResult;
+    }
+}
